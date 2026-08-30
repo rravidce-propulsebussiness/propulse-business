5d1854bcfc13d4981654587adf5c13db8cd00abb
ramavatravi17-hash <ramavatravi17@gmail.com>
2026-08-31 01:28:32 +0530

    Register automatic city sync route

--- backend/src/routes/cityRoutes.js
+++ backend/src/routes/cityRoutes.js
@@ -4,6 +4,7 @@ const cityController = require('../controllers/cityController');
 const router = express.Router();
 
 router.post('/', cityController.createCity);
+router.post('/sync/:stateId', cityController.syncCities);
 router.get('/', cityController.getCities);
 router.get('/:id', cityController.getCityById);
 router.put('/:id', cityController.updateCity);
