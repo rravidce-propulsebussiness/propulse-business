ceb9a37a6e9879179de98a29548fc62b1cc17717
ramavatravi17-hash <ramavatravi17@gmail.com>
2026-08-31 18:49:46 +0530

    Fix leads page header actions

--- frontend/src/admin/pages/AdminLeads.jsx
+++ frontend/src/admin/pages/AdminLeads.jsx
@@ -1,11 +1,1 @@
-import AdminLeadsV9 from './AdminLeadsV9';
-import './AdminLeads.css';
-
-export default function AdminLeads(){
-  return <div className="admin-leads-shell">
-    <div className="sample-lead-toolbar">
-      <a className="sample-lead-download" href="/lead-upload-sample.csv" download="propulse-lead-upload-sample.csv">↓ Sample Lead Sheet</a>
-    </div>
-    <AdminLeadsV9 />
-  </div>;
-}
+export { default } from './AdminLeadsV9';
