ec6dff680eee8f3df68f6a9da9ab8b4a090f5993
ramavatravi17-hash <ramavatravi17@gmail.com>
2026-08-31 14:05:23 +0530

    feat: add pricing rule routes

--- backend/src/routes/leadRoutes.js
+++ backend/src/routes/leadRoutes.js
@@ -1,16 +1,9 @@
-const express = require('express');
-const leadController = require('../controllers/leadController');
-const requireAuth = require('../middleware/authMiddleware');
-const requireAdmin = require('../middleware/adminMiddleware');
-
-const router = express.Router();
-router.get('/', requireAuth, leadController.getLeads);
-router.get('/pricing', requireAdmin, leadController.getLeadPricing);
-router.put('/pricing', requireAdmin, leadController.updateLeadPricing);
-router.get('/:id', requireAuth, leadController.getLeadById);
-router.post('/', requireAdmin, leadController.createLead);
-router.put('/:id', requireAdmin, leadController.updateLead);
-router.patch('/:id/status', requireAdmin, leadController.updateLeadStatus);
-router.delete('/:id', requireAdmin, leadController.deleteLead);
-
-module.exports = router;
+const express=require('express');const leadController=require('../controllers/leadController');const requireAuth=require('../middleware/authMiddleware');const requireAdmin=require('../middleware/adminMiddleware');const router=express.Router();
+router.get('/',requireAuth,leadController.getLeads);
+router.get('/pricing',requireAdmin,leadController.getLeadPricing);
+router.put('/pricing',requireAdmin,leadController.updateLeadPricing);
+router.get('/pricing/rules',requireAdmin,leadController.getPricingRules);
+router.post('/pricing/rules',requireAdmin,leadController.savePricingRule);
+router.put('/pricing/rules/:id',requireAdmin,leadController.savePricingRule);
+router.delete('/pricing/rules/:id',requireAdmin,leadController.deletePricingRule);
+router.get('/:id',requireAuth,leadController.getLeadById);router.post('/',requireAdmin,leadController.createLead);router.put('/:id',requireAdmin,leadController.updateLead);router.patch('/:id/status',requireAdmin,leadController.updateLeadStatus);router.delete('/:id',requireAdmin,leadController.deleteLead);module.exports=router;
