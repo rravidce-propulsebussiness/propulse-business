2e6c61105490b34a20ea590fb55c7862e50d7513
ramavatravi17-hash <ramavatravi17@gmail.com>
2026-09-03 08:04:41 +0530

    Remove redundant lead entitlement routes

--- backend/src/routes/leadEntitlementRoutes.js
+++
@@ -1,7 +0,0 @@
-const express=require('express');
-const requireAuth=require('../middleware/authMiddleware');
-const controller=require('../controllers/leadEntitlementController');
-const router=express.Router();
-router.get('/:id/access',requireAuth,controller.getAccess);
-router.post('/:id/claim',requireAuth,controller.claim);
-module.exports=router;
