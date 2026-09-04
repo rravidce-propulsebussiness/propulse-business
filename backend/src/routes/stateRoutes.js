9f08909a38853d180b7886005975b13fa668c936
ramavatravi17-hash <ramavatravi17@gmail.com>
2026-09-04 11:17:53 +0530

    security: protect state mutations and sync

--- backend/src/routes/stateRoutes.js
+++ backend/src/routes/stateRoutes.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const stateController = require('../controllers/stateController');
+const requireAdmin = require('../middleware/adminMiddleware');
 
 const router = express.Router();
 
-router.post('/', stateController.createState);
 router.get('/', stateController.getStates);
 router.get('/:id', stateController.getStateById);
-router.post('/:id/sync-cities', stateController.syncCities);
-router.put('/:id', stateController.updateState);
-router.delete('/:id', stateController.deactivateState);
+router.post('/', requireAdmin, stateController.createState);
+router.post('/:id/sync-cities', requireAdmin, stateController.syncCities);
+router.put('/:id', requireAdmin, stateController.updateState);
+router.delete('/:id', requireAdmin, stateController.deactivateState);
 
 module.exports = router;
