ba30a4936fee4cfe1bbcbf509385fc529bfe2344
ramavatravi17-hash <ramavatravi17@gmail.com>
2026-09-04 11:13:11 +0530

    feat: add state city sync route

--- backend/src/routes/stateRoutes.js
+++ backend/src/routes/stateRoutes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 router.post('/', stateController.createState);
 router.get('/', stateController.getStates);
 router.get('/:id', stateController.getStateById);
+router.post('/:id/sync-cities', stateController.syncCities);
 router.put('/:id', stateController.updateState);
 router.delete('/:id', stateController.deactivateState);
 
