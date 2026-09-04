ce1ce885d06fde2ad274969e770855ca9c696ad1
ramavatravi17-hash <ramavatravi17@gmail.com>
2026-09-04 09:36:29 +0530

    feat: add city coverage sync route

--- backend/src/routes/cityRoutes.js
+++ backend/src/routes/cityRoutes.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const cityController = require('../controllers/cityController');
-
 const router = express.Router();
-
 router.post('/', cityController.createCity);
 router.post('/sync/:stateId', cityController.syncCities);
 router.get('/', cityController.getCities);
 router.get('/:id', cityController.getCityById);
+router.post('/:id/sync-coverage', cityController.syncCoverage);
 router.put('/:id', cityController.updateCity);
 router.delete('/:id', cityController.deactivateCity);
-
 module.exports = router;
