af3fe83fb2f6d4bdf924ef782f155238c773fa85
rravidce-propulsebussiness <rravi.d.c.e@gmail.com>
2026-09-07 20:35:37 +0530

    Highlight lead discovery on homepage

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import './leadPaymentFix.css'
 import './leadMarketplaceEnhancer.js'
 import './admin/leadImportBulkEnhancer.js'
+import './homeLeadHighlight.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
