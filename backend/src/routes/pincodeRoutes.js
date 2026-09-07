067eaa3ff24ebc56e49ca7927c910c844cf5c420
rravidce-propulsebussiness <rravi.d.c.e@gmail.com>
2026-09-07 12:31:04 +0530

    Add pincode resolve route

--- backend/src/routes/pincodeRoutes.js
+++ backend/src/routes/pincodeRoutes.js
@@ -4,6 +4,7 @@ const pincodeController = require('../controllers/pincodeController');
 const router = express.Router();
 
 router.get('/', pincodeController.search);
+router.get('/resolve', pincodeController.resolve);
 router.get('/:pincode', pincodeController.getOne);
 
 module.exports = router;
