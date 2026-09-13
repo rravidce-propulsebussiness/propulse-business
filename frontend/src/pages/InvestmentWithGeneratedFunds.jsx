b7118bd83e8f79254baf75cdec179b46c3bec4b5
rravidce-propulsebussiness <rravi.d.c.e@gmail.com>
2026-09-13 19:33:12 +0530

    Simplify investor landing page layout

--- frontend/src/pages/InvestmentWithGeneratedFunds.jsx
+++ frontend/src/pages/InvestmentWithGeneratedFunds.jsx
@@ -1,9 +1,5 @@
 import Investment from './Investment'
-import InvestorGeneratedFunds from './InvestorGeneratedFunds'
 
 export default function InvestmentWithGeneratedFunds(){
-  return <>
-    <div className="investment-generated-top"><InvestorGeneratedFunds /></div>
-    <Investment />
-  </>
+  return <Investment />
 }
