13e8ce1ecc64ea2dd6dfdd64dad997efaab8b8f6
rravidce-propulsebussiness <rravi.d.c.e@gmail.com>
2026-09-25 19:51:05 +0530

    Use formula-safe CSV for Lead Partner inventory

--- frontend/src/pages/LeadPartnerInventory.jsx
+++ frontend/src/pages/LeadPartnerInventory.jsx
@@ -2,6 +2,7 @@ import { Fragment, useCallback, useEffect, useRef, useState } from 'react';
 import LeadPartnerSidebar from '../components/LeadPartnerSidebar';
 import { useNavigate } from 'react-router-dom';
 import { authRequest, clearSession, getUser } from '../utils/auth';
+import { downloadCsv } from '../utils/csv';
 import './LeadPartnerHome.css';
 import './LeadPartnerInventory.css';
 
